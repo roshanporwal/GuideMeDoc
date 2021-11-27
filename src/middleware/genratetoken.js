e7f7f6fa3a43c07a3c521cba9dff3c4e44ae714e
nyakole <akolenikhil@gmail.com>
2021-11-27 18:50:14 +0530

    token time changes to 7 days

--- src/middleware/genratetoken.js
+++ src/middleware/genratetoken.js
@@ -11,7 +11,7 @@ process.env.TOKEN_SECRET;
     try{
 
     
-    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '15552000s' });
+    return jwt.sign(username, process.env.TOKEN_SECRET, { expiresIn: '604800s' });
     }catch(err) {
       return res.status(404).json({ error: err, message: "something went wrong pls check filed" })
     } 
