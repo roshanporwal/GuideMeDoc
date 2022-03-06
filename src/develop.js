5c41a403da5576d0cbb88ca65d5956fe75b4f57f
nishant2106 <nishantpandey2106@gmail.com>
2022-03-06 13:08:50 +0530

    https

--- src/develop.js
+++ src/develop.js
@@ -5,9 +5,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const util = require('util');
 const path = require('path');
-const fs = require('fs');
+var https = require('https');
+var debug = require('debug')('bkserver:server');
+var fs = require('fs');
 const cors = require('cors');
-
+// var http = require('http');
 
 
 connectDB();
@@ -30,4 +32,66 @@ app.use(cors());
 
 
 const Port = process.env.Port || 8082;
-app.listen(Port, () => console.log('Server started'));
+// app.listen(Port, () => console.log('Server started'));
+
+
+// var server = http.createServer(app);
+
+// server.listen(Port);
+// server.on('error', onError);
+// server.on('listening', onListening)
+app.set('secPort',Port+443);
+
+var options = {
+    // key: fs.readFileSync(__dirname+'/private.key'),
+    // cert: fs.readFileSync(__dirname+'/certificate.pem')
+    key: fs.readFileSync(__dirname+'/privkey.pem'),
+    cert: fs.readFileSync(__dirname+'/fullchain.pem')
+  };
+
+  var secureServer = https.createServer(options,app);
+  secureServer.listen(app.get('secPort'), () => {
+    console.log('Server listening on port ',app.get('secPort'));
+ });
+ secureServer.on('error', onError);
+ secureServer.on('listening', onListening);
+
+ /**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    var bind = typeof Port === 'string'
+      ? 'Pipe ' + Port
+      : 'Port ' + Port;
+  
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+  
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+  
+  function onListening() {
+    var addr = secureServer.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.Port;
+    debug('Listening on ' + bind);
+  }
