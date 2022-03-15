1affb554fd22f14186da07c497fea55e7d7116f9
nishant2106 <nishantpandey2106@gmail.com>
2022-03-15 22:39:32 +0530

    connection

---
+++ src/DB/.Connection.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const URI ="mongodb://admin:admin@cluster0-shard-00-00.elcbh.mongodb.net:27017,cluster0-shard-00-01.elcbh.mongodb.net:27017,cluster0-shard-00-02.elcbh.mongodb.net:27017/development?ssl=true&replicaSet=atlas-hueon9-shard-0&authSource=admin&retryWrites=true&w=majority";
+
+const connectDB = async () => {
+ 
+  await mongoose.connect(URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true
+  });
+  console.log('db connected.!');
+};
+
+module.exports = connectDB;
